0766590f9de832ebb71a9da59abac5634ee2386c
Jade Edward <jade@jadeedward.com>
2021-12-10 13:55:58 +1100

    Try a different way of doing the settings

--- apps/widmpsh/settings.js
+++ apps/widmpsh/settings.js
@@ -4,23 +4,23 @@
     if (settings.orientation===undefined) settings.orientation="North";
     return settings;
   }
+
   function updateSetting(setting, value) {
     let settings = require('Storage').readJSON("widpmsh.settings.json", true) || {};
     settings[setting] = value;
     require('Storage').writeJSON("widpmsh.settings.json", settings);
+    if ("widmpsh" in WIDGETS) WIDGETS["widmpsh"].draw();
   }
 
-  var orientations = ["North", "South"];
-  var currentOrientation = settings().orientation;
   var mainmenu = {
     "" : { "title" : "Messages" },
     "< Back" : back,
-    'Orientation': {
-      value: settings().orientation,
-      format: o => orientations[o],
-      onchange: o => {
-        updateSetting("orientation", orientations[o]);
-      }
+    "Orientation": {
+      value: settings.orientation|0,
+      format: v => ["North", "South", "Equator"][v%3],
+      onchange: v => {
+        updateSetting('orientation', v%3);
+      },
     },
   };
   E.showMenu(mainmenu);
