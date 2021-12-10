fd3c2821cb569219f36626ee246fcd09f9bcb39e
Jade Edward <jade@jadeedward.com>
2021-12-10 13:21:00 +1100

    Add data for cleanup, fix settings

--- apps/widmpsh/settings.js
+++ apps/widmpsh/settings.js
@@ -1,25 +1,28 @@
 (function(back) {
-  const ORIENTATIONS = ['North', 'South', 'Equator'];
-  let settings = require('Storage').readJSON('widmpsh.json',1)||{};
-  if (typeof settings.orientation !== "string") settings.orientation = ORIENTATIONS[0]; // default value
-  function save(key, value) {
-    settings[key] = value;
-    require('Storage').write('widmpsh.json', settings);
+  function settings() {
+    let settings = require('Storage').readJSON("widpmsh.settings.json", true) || {};
+    if (settings.orientation===undefined) settings.orientation="North";
+    return settings;
   }
-  const appMenu = {
-    '': {'title': 'Moon Orientation'},
-    '< Back': back,
+  function updateSetting(setting, value) {
+    let settings = require('Storage').readJSON("widpmsh.settings.json", true) || {};
+    settings[setting] = value;
+    require('Storage').writeJSON("widpmsh.settings.json", settings);
+  }
+
+  var orientations = ["North", "South"];
+  var currentOrientation = settings().orientation;
+  var mainmenu = {
+    "" : { "title" : "Messages" },
+    "< Back" : back,
     'Orientation': {
-      value: settings.orientation,
-      format: () => settings.orientation,
-      onchange: function () {
-        // cycles through options
-        const oldIndex = ORIENTATIONS.indexOf(settings.orientation)
-        const newIndex = (oldIndex + 1) % ORIENTATIONS.length
-        settings.orientation = ORIENTATIONS[newIndex]
-        save('orientation')(settings.orientation)
+      value: Math.max(0,orientations.indexOf(settings().orientation)),
+      min: 0, max: orientations.length,
+      format: o => orientations[o]||"Off",
+      onchange: o => {
+        updateSetting("orientation", orientations[v]);
       }
-    }
+    },
   };
-  E.showMenu(appMenu)
+  E.showMenu(mainmenu);
 })
