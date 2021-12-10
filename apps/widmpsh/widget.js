061d7aa95c25a29a2986498e70f47588f7c61af3
Jade Edward <jade@jadeedward.com>
2021-12-10 13:32:55 +1100

    Implemented hemisphere difference

--- apps/widmpsh/widget.js
+++ apps/widmpsh/widget.js
@@ -1,25 +1,63 @@
-WIDGETS["widmoonsh"] = { area: "tr", width: 24, draw: function() {
-  const MC = 29.5305882, NM = 694039.09;
-  var r = 11, mx = this.x + 12; my = this.y + 12;
+(() => {
+  const s = require('Storage');
+  const SETTINGS_FILE = 'widmpsh.settings.json';
 
-  function moonPhase(d) {
-    var tmp, month = d.getMonth(), year = d.getFullYear(), day = d.getDate();
-    if (month < 3) {year--; month += 12;}
-    tmp = ((365.25 * year + 30.6 * ++month + day - NM) / MC);
-    return Math.round(((tmp - (tmp | 0)) * 7)+1);
+  let settings;
+
+  //load settings
+  function loadSettings() {
+    settings = s.readJSON(SETTINGS_FILE, 1) || {};
   }
 
-  const BLACK = g.theme.bg, MOON = 0x41f;
-  var moon = {
-    0: () => { g.reset().setColor(BLACK).fillRect(mx - r, my - r, mx + r, my + r);},
-    1: () => { moon[0](); g.setColor(MOON).drawCircle(mx, my, r);},
-    2: () => { moon[3](); g.setColor(BLACK).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);},
-    3: () => { moon[0](); g.setColor(MOON).fillCircle(mx, my, r).setColor(BLACK).fillRect(mx, my - r, mx + r, my + r);},
-    4: () => { moon[3](); g.setColor(MOON).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);},
-    5: () => { moon[0](); g.setColor(MOON).fillCircle(mx, my, r);},
-    6: () => { moon[7](); g.setColor(MOON).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);},
-    7: () => { moon[0](); g.setColor(MOON).fillCircle(mx, my, r).setColor(BLACK).fillRect(mx - r, my - r, mx, my + r);},
-    8: () => { moon[7](); g.setColor(BLACK).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);}
-  };
-  moon[moonPhase(Date())]();
-} };
+  //return setting
+  function setting(key) {
+    //define default settings
+    const DEFAULTS = {
+      'orientation' : 'North',
+    };
+    if (!settings) { loadSettings(); }
+    return (key in settings) ? settings[key] : DEFAULTS[key];
+  }
+
+  WIDGETS["widmoonsh"] = { area: "tr", width: 24, draw: function() {
+    const MC = 29.5305882, NM = 694039.09;
+    var r = 11, mx = this.x + 12; my = this.y + 12;
+
+    function moonPhase(d) {
+      var tmp, month = d.getMonth(), year = d.getFullYear(), day = d.getDate();
+      if (month < 3) {year--; month += 12;}
+      tmp = ((365.25 * year + 30.6 * ++month + day - NM) / MC);
+      return Math.round(((tmp - (tmp | 0)) * 7)+1);
+    }
+
+    const BLACK = g.theme.bg, MOON = 0x41f;
+
+    if (setting('orientation') === 'South') {
+      var moon = {
+        0: () => { g.reset().setColor(BLACK).fillRect(mx - r, my - r, mx + r, my + r);},
+        1: () => { moon[0](); g.setColor(MOON).drawCircle(mx, my, r);},
+        2: () => { moon[3](); g.setColor(BLACK).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);},
+        3: () => { moon[0](); g.setColor(MOON).fillCircle(mx, my, r).setColor(BLACK).fillRect(mx, my - r, mx + r, my + r);},
+        4: () => { moon[3](); g.setColor(MOON).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);},
+        5: () => { moon[0](); g.setColor(MOON).fillCircle(mx, my, r);},
+        6: () => { moon[7](); g.setColor(MOON).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);},
+        7: () => { moon[0](); g.setColor(MOON).fillCircle(mx, my, r).setColor(BLACK).fillRect(mx - r, my - r, mx, my + r);},
+        8: () => { moon[7](); g.setColor(BLACK).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);}
+      };
+    } else {
+      var moon = {
+        0: () => { g.reset().setColor(BLACK).fillRect(mx - r, my - r, mx + r, my + r);},
+        1: () => { moon[0](); g.setColor(MOON).drawCircle(mx, my, r);},
+        2: () => { moon[3](); g.setColor(BLACK).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);},
+        3: () => { moon[0](); g.setColor(MOON).fillCircle(mx, my, r).setColor(BLACK).fillRect(mx - r, my - r, mx, my + r);},
+        4: () => { moon[3](); g.setColor(MOON).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);},
+        5: () => { moon[0](); g.setColor(MOON).fillCircle(mx, my, r);},
+        6: () => { moon[7](); g.setColor(MOON).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);},
+        7: () => { moon[0](); g.setColor(MOON).fillCircle(mx, my, r).setColor(BLACK).fillRect(mx, my - r, mx + r, my + r);},
+        8: () => { moon[7](); g.setColor(BLACK).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);}
+      };
+    }
+    moon[moonPhase(Date())]();
+  } };
+
+})();
